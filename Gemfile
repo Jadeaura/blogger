51fffc9866656eca52250a6929ca3c550e9e978a
Keegan Provo <keeganprovo@gmail.com>
2016-10-05 19:01:21 -0600

    Added Sorcery authentication

--- Gemfile
+++ Gemfile
@@ -35,6 +35,9 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Images and stuff
 gem "paperclip"
 
+# Authentication
+gem "sorcery"
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,4 +5,14 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
   end
+
+  before_filter :require_login, only: [:destroy]
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+
+    flash.notice = "Tag '#{@tag.name}' Deleted!"
+
+    redirect_to tags_path
+  end
 end
